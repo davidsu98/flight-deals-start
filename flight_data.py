48416d9647aa64afdf923195fa74f309984da75b
davidsu98 <97572090+davidsu98@users.noreply.github.com>
2023-09-06 21:44:09 -0700

    Lowest flight price for the next year function

--- flight_data.py
+++ flight_data.py
@@ -4,10 +4,17 @@ import datetime
 import requests
 
 class FlightData:
-    def search(start, end, date=str):
-        flight_data_raw=requests.get(url = tequila_url+'search', params={'fly_from':start, 'fly_to':end, 'date_from':date, 'date_to':date, 'adults':1, 'curr':'CAD', 'vehicle_type':'aircraft'}, headers=API_KEY_TEQUILA)
+
+    #returns lowest price of flights between dates (1 year from tomorrow)
+    def lowest_year(start, end):
+        now = datetime.datetime.now()+datetime.timedelta(days=1)
+        date1 = now.strftime('%d/%m/%Y')
+        date2 = (now+datetime.timedelta(days=365)).strftime('%d/%m/%Y')
+
+        flight_data_raw=requests.get(url = tequila_url+'search', params={'fly_from':start, 'fly_to':end, 'date_from':date1, 'date_to':date2, 'adults':1, 'curr':'CAD', 'vehicle_type':'aircraft'}, headers=API_KEY_TEQUILA)
         flight_data = flight_data_raw.json()['data']
-        print(str(flight_data[0]['price']) + ' you r so poor to fly with: ' + flight_data[0]['airlines'][0])
+        return flight_data[0]['price']
+        #return the date of the cheapest flight
 
 
     #get data from google sheets on start and end cities. refills sheets with price?
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from data_manager import DataManager
 from flight_data import FlightData
 import datetime
 
-start_city = input('Which city would you like to fly from?: ')
-date = input('Please input date (dd/mm/yyyy): ')
+# start_city = input('Which city would you like to fly from?: ')
+# date = input('Please input date (dd/mm/yyyy): ')
 
-top_des_city = DataManager.starting_loc(start_city)
+# top_des_city = DataManager.starting_loc(start_city)
 
-DataManager.fill_sheet(top_des_city, start_city, date)
+# DataManager.fill_sheet(top_des_city, start_city, date)
+
+print(FlightData.lowest_year('YVR', 'YYZ'))
