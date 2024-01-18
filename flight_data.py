1ec527e8e60d38980e6548f5107a20161248a190
davidsu98 <97572090+davidsu98@users.noreply.github.com>
2024-01-18 14:47:32 -0800

    Update

--- flight_data.py
+++ flight_data.py
@@ -15,7 +15,7 @@ class FlightData:
         flight_data_raw=requests.get(url = tequila_url+'search', params={'fly_from':start, 'fly_to':end, 'date_from':date1, 'date_to':date2, 'adults':1, 'curr':'CAD', 'vehicle_type':'aircraft', 'limit':10, 'max_stopovers':0}, headers=API_KEY_TEQUILA)
         flight_data = flight_data_raw.json()['data']
 
-        return [(flight_data[i]['price'],datetime.fromtimestamp(flight_data[i]['dTime']).strftime('%Y-%m-%d')) for i in range(len(flight_data))] 
+        return [(datetime.fromtimestamp(flight_data[i]['dTime']).strftime('%Y-%m-%d'), '$'+str(flight_data[i]['price'])+'CAD') for i in range(len(flight_data))] 
 
     #get data from google sheets on start and end cities. refills sheets with price?
     #do all at the same time. start end price?
--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from data_manager import DataManager
 from flight_data import FlightData
 import datetime
 
-start_city = input('Which city would you like to fly from?: ')
-date = input('Please input date (dd/mm/yyyy): ')
+start_city = input('Which city would you like to fly from? (IATA): ')
+
+
 
 top_des_city = DataManager.starting_loc(start_city)
 
-DataManager.fill_sheet(top_des_city, start_city, date)
-select_city = input(f'Please select a city from the following list:\n{[(top_des_city[i]["name"],top_des_city[i]["code"]) for i in range(len(top_des_city))]}\n')
+#   the following function fills an excel sheet using sheety [top destination, iata, price]
+#   DataManager.fill_sheet(top_des_city, start_city, date)
 
+#destination selection
+city_select = input('Please enter your preferred Destination (IATA) or if you want to choose from Suggested Destinations, type "continue":\n')
 
-print(FlightData.lowest_year(start_city, select_city))
+if city_select == 'continue':
+    select_city = input(f'Select a Top 10 Destinations from {start_city}:\n{[(top_des_city[i]["name"],top_des_city[i]["code"]) for i in range(len(top_des_city))]}\nOr if destination not listed input your own (IATA):\n')
+else:
+    select_city = city_select
+#date selection
+date = input('Please input date (dd/mm/yyyy) or "continue" to see cheapest options:\n')
+
+if date == 'continue':
+    print(f'Displaying cheapest dates to fly in the upcoming 365 days:\n{FlightData.lowest_year(start_city, select_city)}')
+else:
+    print('this function not ready')
+
+#make the selection able to backtrack, "reselect", "back", "cancel", etc
